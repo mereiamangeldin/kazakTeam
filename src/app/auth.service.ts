8678efb2c8bd385df5a0ca8d23fba5f3e5826922
mereiamangeldin <me_amangeldin@kbtu.kz>
2023-05-03 22:53:34 +0600

    maksat

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    return true;
     // Проверить наличие токена в localStorage
     const token = this.getToken();
     return token != null;
